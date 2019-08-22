083c18dc7affd5fef985c27cb8d3e1f908923d25
Clement Baudonnel <33254234+Skyrize@users.noreply.github.com>
2019-08-22 17:36:44 +0200

    [Bug fix] (Master)
    Fixed queue bug, now philosophers walks properly and go to the Parthenon, then get Destroyed.

--- The Hoplite Lesson/Assets/Scripts/PhilosopherComponent.cs
+++ The Hoplite Lesson/Assets/Scripts/PhilosopherComponent.cs
@@ -8,11 +8,14 @@ public class PhilosopherComponent : MonoBehaviour
     public GameObject question;
     // Start is called before the first frame update
     private GameObject path;
+
+    private GameObject entry;
     void Start()
     {
         question.SetActive(false);
         path = GameObject.Find("Path");
         hoplite = GameObject.Find("Hoplite");
+        entry = GameObject.Find("Entry");
     }
 
     public void AskQuestion()
@@ -24,32 +27,42 @@ public class PhilosopherComponent : MonoBehaviour
 
     public void LeaveWaypoint()
     {
-        path.GetComponent<PathComponent>().wayPoints[path.GetComponent<PathComponent>().wayPoints.Length - 1].GetComponent<WaypointComponent>().isOccupied = false;
+        GetComponent<PathFollowingComponent>().wayPoint.GetComponent<WaypointComponent>().isOccupied = false;
+        GetComponent<PathFollowingComponent>().wayPoint = null;
     }
 
     public void GetKickedOut()
     {
+        transform.parent = null;
         LeaveWaypoint();
         //animate
         Debug.Log("Get Kicked out !");
         GetComponent<Rigidbody2D>().simulated = true;
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 10));
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(-100, 100));
     }
 
+    private bool isAllowedInside = false;
     public void GetInside()
     {
+        transform.parent = null;
         LeaveWaypoint();
         Debug.Log("Get inside !");
-        GetComponent<Rigidbody2D>().simulated = true;
-        //animate
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 10));
-
+        isAllowedInside = true;
     }
 
     // Update is called once per frame
+    private float speed = 5;
     void Update()
     {
         if (question.activeInHierarchy == false && Vector3.Distance(transform.position, path.GetComponent<PathComponent>().wayPoints[path.GetComponent<PathComponent>().wayPoints.Length - 1].transform.position) < 0.001f)
             AskQuestion();
+        if (isAllowedInside) {
+            if (Vector3.Distance(transform.position, entry.transform.position) < 0.001f) {
+                Debug.Log("Destroy");
+                Destroy(this.gameObject);
+            } else {
+                transform.position = Vector3.MoveTowards(transform.position, entry.transform.position, speed * Time.deltaTime);
+            }
+        }
     }
 }
