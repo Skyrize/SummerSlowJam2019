47d67a3c8c0591d955ec33adebaee50a9065e270
Choz <remy.sainsard@gmail.com>
2019-08-22 17:39:46 +0200

    sprite update

--- The Hoplite Lesson/Assets/Sprites/0_sprites_export.bat
+++ The Hoplite Lesson/Assets/Sprites/0_sprites_export.bat
@@ -1,12 +1,13 @@
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_idle.aseprite --scale 2 --sheet philosopher1_idle_x2.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_idle.aseprite --scale 4 --sheet philosopher1_idle_x4.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_idle.aseprite --scale 2 --sheet philosopher2_idle_x2.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_idle.aseprite --scale 4 --sheet philosopher2_idle_x4.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_idle.aseprite --scale 2 --sheet philosopher1_idle_x2.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_idle.aseprite --scale 4 --sheet philosopher1_idle_x4.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_idle.aseprite --scale 2 --sheet philosopher2_idle_x2.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_idle.aseprite --scale 4 --sheet philosopher2_idle_x4.png
 
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_walk.aseprite --scale 2 --sheet philosopher1_walk_x2.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_walk.aseprite --scale 4 --sheet philosopher1_walk_x4.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_walk.aseprite --scale 2 --sheet philosopher2_walk_x2.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_walk.aseprite --scale 4 --sheet philosopher2_walk_x4.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_walk.aseprite --scale 2 --sheet philosopher1_walk_x2.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_walk.aseprite --scale 8 philosopher1_walk_x8.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_walk.aseprite --scale 2 --sheet philosopher2_walk_x2.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_walk.aseprite --scale 4 --sheet philosopher2_walk_x4.png
 
-"C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 2 --sheet hoplite_idle_x2.png
-"C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 4 --sheet hoplite_idle_x4.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 2 --sheet hoplite_idle_x2.png
+rem "C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 4 --sheet hoplite_idle_x4.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 8 hoplite_idle_x8.png
