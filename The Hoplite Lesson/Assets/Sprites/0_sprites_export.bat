dd78a55cbe5601bce3f188ddfe2ff122df9d2f5f
Choz <remy.sainsard@gmail.com>
2019-08-22 15:11:46 +0200

    Sprites added

---
+++ The Hoplite Lesson/Assets/Sprites/0_sprites_export.bat
@@ -0,0 +1,12 @@
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_idle.aseprite --scale 2 --sheet philosopher1_idle_x2.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_idle.aseprite --scale 4 --sheet philosopher1_idle_x4.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_idle.aseprite --scale 2 --sheet philosopher2_idle_x2.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_idle.aseprite --scale 4 --sheet philosopher2_idle_x4.png
+
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_walk.aseprite --scale 2 --sheet philosopher1_walk_x2.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher1_walk.aseprite --scale 4 --sheet philosopher1_walk_x4.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_walk.aseprite --scale 2 --sheet philosopher2_walk_x2.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b philosopher2_walk.aseprite --scale 4 --sheet philosopher2_walk_x4.png
+
+"C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 2 --sheet hoplite_idle_x2.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b hoplite_idle.aseprite --scale 4 --sheet hoplite_idle_x4.png
