cfa9ff104b111931ca23b70c4ec640635e7b5428
Choz <remy.sainsard@gmail.com>
2019-08-23 23:28:44 +0200

    SampleScene revert

--- The Hoplite Lesson/Assets/Scripts/SceneSwitcher.cs
+++ The Hoplite Lesson/Assets/Scripts/SceneSwitcher.cs
@@ -17,6 +17,6 @@ public class SceneSwitcher : MonoBehaviour
 
     public void GotoGameScene()
     {
-    	SceneManager.LoadScene("GameScene");
+    	SceneManager.LoadScene("SampleScene");
     }
 }
