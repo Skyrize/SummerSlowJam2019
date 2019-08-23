8ad7603268a7878ac68089fc9a78edf8fe1e97a6
Choz <remy.sainsard@gmail.com>
2019-08-23 23:00:37 +0200

    Menu and Credit Scenes

---
+++ The Hoplite Lesson/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void GotoCreditsScene()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void GotoGameScene()
+    {
+    	SceneManager.LoadScene("GameScene");
+    }
+}
